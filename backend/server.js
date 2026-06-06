ba70d794fd184cebd70d3d4ae5696f34ee7e0ddb
Premkumar Bhadagave <pgbhadagavepg@gmail.com>
2026-06-06 23:45:33 +0530

    "deployment"

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,28 @@ const messageRouter=require('./router/messageRouter')
 const PORT = process.env.PORT || 5000
 const app=express()
 app.use(express.json())
+const path= require('path')
 
 connectDB()
-app.get('/',(req,res)=>{
-    res.send("HEllo world")
-})
+
 app.use('/api/user',userRouter)
 app.use('/api/chat',chatsRouter)
 app.use('/api/message',messageRouter)
+
+//---------------------Deployment----------------------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send("API is RUNNING SUCCESSFULLY");
+    });
+}
+//---------------------Deployment----------------------------
 app.use(notFound);
 app.use(errorHandler) ;
 
